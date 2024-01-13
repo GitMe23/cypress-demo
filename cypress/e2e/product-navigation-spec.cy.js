6d749686c390dddf2109d9303159dc8f1b6ec870
AH <andyhop@hotmail.co.uk>
2024-01-13 10:26:10 +0000

    Moved getProductRanges to before all hook

--- cypress/e2e/product-navigation-spec.cy.js
+++ cypress/e2e/product-navigation-spec.cy.js
@@ -2,37 +2,38 @@
 // 1: The user can navigate to the page of a specific product range
 // 2: The user can see the list of all products in the product range
 
-describe('navigating product ranges', () => {
+describe('Navigating Product Ranges', () => {
+  let productRanges;
+
+  before(() => {
+    // Fetch product ranges once before all tests
+    cy.getProductRanges().then((ranges) => {
+      productRanges = ranges;
+    });
+  });
 
   beforeEach(() => {
-    cy.visit('/')
-  })
+    cy.visit('/');
+  });
 
   it("displays 'Focusrite Downloads'", () => {
     cy.get('#block-downloads-page-title')
-    .contains('Focusrite Downloads')
-    .should('be.visible');
-  })
+      .contains('Focusrite Downloads')
+      .should('be.visible');
+  });
 
   it("displays correct number of Focusrite product ranges", () => {
-    cy.getProductRanges().then((productRanges) => {
-      cy.get('[class="tile"]')
+    cy.get('[class="tile"]')
       .should('have.length', productRanges.length);
-    });
-  })
+  });
 
   it("makes all product ranges visible and clickable", () => {
-    cy.getProductRanges().then((productRanges) => {
-      productRanges.forEach(productRange => {
-        cy.contains(productRange).should('be.visible');
-        cy.contains(productRange)
+    productRanges.forEach(productRange => {
+      cy.contains(productRange).should('be.visible');
+      cy.contains(productRange)
         .parents('.tile[data-once="clickable-elements-click"]')
         .should('be.visible')
-        .should('have.attr', 'data-once', 'clickable-elements-click') 
-      })
-    })
-  })
-
-})
-
-
+        .should('have.attr', 'data-once', 'clickable-elements-click');
+    });
+  });
+});
