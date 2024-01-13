9b2e32a20a91105821cd8507d91de3e680483213
AH <andyhop@hotmail.co.uk>
2024-01-13 10:22:40 +0000

    Added displays header, correct no of ranges, makes all ranges visible and clickable

--- cypress/e2e/product-navigation-spec.cy.js
+++ cypress/e2e/product-navigation-spec.cy.js
@@ -2,16 +2,37 @@
 // 1: The user can navigate to the page of a specific product range
 // 2: The user can see the list of all products in the product range
 
-describe('Downloads navigation', () => {
+describe('navigating product ranges', () => {
+
   beforeEach(() => {
     cy.visit('/')
   })
-  it("clearly displays 'Focusrite Downloads'", () => {
-    cy.contains('Focusrite Downloads');
+
+  it("displays 'Focusrite Downloads'", () => {
+    cy.get('#block-downloads-page-title')
+    .contains('Focusrite Downloads')
+    .should('be.visible');
   })
+
   it("displays correct number of Focusrite product ranges", () => {
     cy.getProductRanges().then((productRanges) => {
-      cy.get('[class="tile"]').should('have.length', productRanges.length);
-  });
+      cy.get('[class="tile"]')
+      .should('have.length', productRanges.length);
+    });
+  })
+
+  it("makes all product ranges visible and clickable", () => {
+    cy.getProductRanges().then((productRanges) => {
+      productRanges.forEach(productRange => {
+        cy.contains(productRange).should('be.visible');
+        cy.contains(productRange)
+        .parents('.tile[data-once="clickable-elements-click"]')
+        .should('be.visible')
+        .should('have.attr', 'data-once', 'clickable-elements-click') 
+      })
+    })
+  })
+
 })
-});
+
+
